9ae3a911040aac36a040b991d33a0ca0ecf695c5
Oleksiy Reseskul <alvarus@ua.fm>
2020-02-25 00:12:48 +0200

    Add task "Trianle by loop for" - Completed

---
+++ Lesson_06/Triangles.py
@@ -0,0 +1,73 @@
+rows = int(input('\nВведите высоту фигуры:\t'))
+print()
+cols = (rows * 2) - 1
+
+# ---------------------------------------------
+
+print(' "A"')
+
+for i in range(rows):
+    for j in range(cols):
+        if rows - 1 - i == j or j == rows - 1 + i or i == rows - 1:
+            print('* ', end='')
+        else:
+            print('  ', end='')
+    print()
+print()
+
+# ---------------------------------------------
+
+print(' "B"')
+
+for i in range(rows):
+    for j in range(cols):
+        if rows - 1 - i <= j <= rows - 1 + i:
+            print('* ', end='')
+        else:
+            print('  ', end='')
+    print()
+print()
+
+# ---------------------------------------------
+
+print(' "C"')
+
+for i in range(rows):
+    for j in range(cols):
+        if rows - 1 - i <= j <= rows - 1 + i:
+            print('* ', end='')
+        else:
+            print('  ', end='')
+    print()
+for i in range(rows):
+    if i == 0:
+        continue
+    for j in range(cols):
+        if i == j or i == cols - j - 1:
+            print('* ', end='')
+        else:
+            print('  ', end='')
+    print()
+print()
+
+# ---------------------------------------------
+
+print(' "D"')
+
+for i in range(rows):
+    for j in range(cols):
+        if rows - 1 - i <= j <= rows - 1 + i:
+            print('* ', end='')
+        else:
+            print('  ', end='')
+    print()
+for i in range(rows):
+    if i == 0:
+        continue
+    for j in range(cols):
+        if i == j or i == cols - j - 1 or j == cols // 2:
+            print('* ', end='')
+        else:
+            print('  ', end='')
+    print()
+print()
