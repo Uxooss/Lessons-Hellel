b41139b83dd02a63163737ff99f3cfb6afa9cbbc
Oleksiy Reseskul <alvarus@ua.fm>
2020-03-03 23:19:05 +0200

    Corrected / Modified

--- Lesson_06/6.1 - Triangles.py
+++ Lesson_06/6.1 - Triangles.py
@@ -30,20 +30,29 @@ print()
 
 # ---------------------------------------------
 
+if (rows % 2) == 0:
+    rows = rows // 2
+    cols = (rows * 2) - 1
+else:
+    rows = (rows // 2) + 1
+    cols = (rows * 2) - 1
+
+# ---------------------------------------------
+
 print(' "C"')
 
-for i in range(rows // 2):
-    for j in range(cols // 2):
-        if (rows // 2) - 1 - i <= j <= (rows // 2) - 1 + i:
+for i in range(rows):
+    for j in range(cols):
+        if rows - 1 - i <= j <= rows - 1 + i:
             print('* ', end='')
         else:
             print('  ', end='')
     print()
-for i in range(rows // 2):
+for i in range(rows):
     if i == 0:
         continue
-    for j in range(cols // 2):
-        if i == j or i == (cols // 2) - j - 1:
+    for j in range(cols):
+        if i == j or i == cols - j - 1:
             print('* ', end='')
         else:
             print('  ', end='')
@@ -54,18 +63,18 @@ print()
 
 print(' "D"')
 
-for i in range(rows // 2):
-    for j in range(cols // 2):
-        if (rows // 2) - 1 - i <= j <= (rows // 2) - 1 + i:
+for i in range(rows):
+    for j in range(cols):
+        if rows - 1 - i <= j <= rows - 1 + i:
             print('* ', end='')
         else:
             print('  ', end='')
     print()
-for i in range(rows // 2):
+for i in range(rows):
     if i == 0:
         continue
-    for j in range(cols // 2):
-        if i == j or i == (cols // 2) - j - 1 or j == (cols // 4):
+    for j in range(cols):
+        if i == j or i == cols - j - 1 or j == cols // 2:
             print('* ', end='')
         else:
             print('  ', end='')
